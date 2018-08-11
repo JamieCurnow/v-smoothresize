6e83919ed89e87be4ee716bbfde25d3d6613f454
JamieCurnow <jamie@island-web.ca>
2018-08-11 16:42:08 -0700

    Initial commit with a basic working directive

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # v-smoothresize
 A smooth resizing directive for vue
+
+[See an example here on codepen](https://codepen.io/JamieCurnow/pen/YjBOmY?editors=1000)
+
+# Contribution
+Yes please! This is just the start. If anyone wants to wrap this up into a plugin or for publishing on NPM be my guest! All pull requests more than welcome.
---
+++ smoothResize.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+
+Vue.directive('smoothResize', {
+  update (el, binding, vnode) {
+    el.style.height = 'auto'
+    let transitionTime = (binding.value && binding.value.transition) || 300
+    let delay = (binding.value && binding.value.delay) || 100
+    let oldHeight = `${el.clientHeight}px`
+    // set the transition and overflow styles.
+    el.style.transition = `all ${transitionTime}ms cubic-bezier(0.25, 0.8, 0.5, 1)`
+    el.style.overflow = 'hidden'
+    // wait for nextTick to get new height
+    vnode.context.$nextTick(() => {
+      let newHeight = `${el.clientHeight}px`
+      // set the height of the element to the old height
+      // this should happen before any visible changes
+      el.style.height = oldHeight
+      // use setTimeout to delay setting the element's height to the new height
+      setTimeout(() => {
+        el.style.height = newHeight
+      }, delay)
+    })
+  }
+})
