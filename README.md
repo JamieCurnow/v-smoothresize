4ca5a9d2e4562555d7cc116ff35e1ccee490951f
JamieCurnow <jamie@island-web.ca>
2018-08-21 15:00:04 -0700

    Updating the directive to use a requestAnimationFrame() loop. Working way better now. Also added a new fineTune prop to use to set how many browser repaints it waits for before abandoning. Also updated readme.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# v-smoothresize
+# v-smooth-resize
 A smooth resizing directive for vue
 
-[See an example here on codepen](https://codepen.io/JamieCurnow/pen/YjBOmY?editors=1000)
+[See an example here on codepen](https://codepen.io/JamieCurnow/full/YjBOmY/)
 
 # Contribution
 Yes please! This is just the start. If anyone wants to wrap this up into a plugin or for publishing on NPM be my guest! All pull requests more than welcome.
@@ -9,13 +9,19 @@ Yes please! This is just the start. If anyone wants to wrap this up into a plugi
 # Usage
 Use the directive in your project - see [Vue - Custom Directives](https://vuejs.org/v2/guide/custom-directive.html)
 
-Add the `v-smoothresize` directive to any element that should be resized. Optionally an object can be used to fine-tune the resizing transition length of time and also to add a delay if desired.
+Add the `v-smooth-resize` directive to any element that should be resized smoothly when child nodes are updated. Optionally an object can be used to fine-tune the resizing, change the transition length of time and also to add a delay if desired.
+
+`fineTune` = the amount of browser repaints to wait for before we abandon the smooth resize. Useful if the children have transitions that last more than 20 browser repaints. Default is `20`, creep up from there - if it's too high, it will max the call stack.
+
+`transition` = set the transition time for the change of height in `ms`. Useful for elements with large children so that the resize animation doesn't seem really fast. Default is `300`
+
+`delay` = set a delay time in `ms` once we have caught the resize event and before actually changing the size of the element. This could be useful for children that take time to load once mounted. Default is `0`.
 
 ```
-<div v-smoothresize="{transition: 200, delay: 100}">
+<div v-smoothresize="{fineTune: 27, transition: 400, delay: 100}">
   <div>This will always be here</div>
   <div v-if="someCondition">This will appear and the parent will grow smoothly</div>
 </div>
 ```
 
-[Check out the example for a clearer idea of how it works.](https://codepen.io/JamieCurnow/pen/YjBOmY?editors=1000)
+[Check out the example for a clearer idea of how it works.](https://codepen.io/JamieCurnow/full/YjBOmY/)
--- v-smoothresize.js
+++ v-smoothresize.js
@@ -1,24 +1,73 @@
 import Vue from 'vue'
 
-Vue.directive('smoothresize', {
-  update (el, binding, vnode) {
-    el.style.height = 'auto'
-    let transitionTime = (binding.value && binding.value.transition) || 300
-    let delay = (binding.value && binding.value.delay) || 100
-    let oldHeight = `${el.clientHeight}px`
-    // set the transition and overflow styles.
-    el.style.transition = `all ${transitionTime}ms cubic-bezier(0.25, 0.8, 0.5, 1)`
-    el.style.overflow = 'hidden'
-    // wait for nextTick to get new height
-    vnode.context.$nextTick(() => {
-      let newHeight = `${el.clientHeight}px`
-      // set the height of the element to the old height
-      // this should happen before any visible changes
+const checkHeightForChangeAndUpdate = (el, oldHeight, delay, transitionTime, fineTune) => {
+  // Set the max amount of times to check for new height just before browser repaint.
+  let maxCheck = fineTune
+  // keep track of the current check number
+  let currentCheck = 0
+  // function to check and loop until currentCheck === maxCheck
+  const check = () => {
+    // check to see if this should run and return if not
+    if (currentCheck === maxCheck) { return false }
+    // clock up currentCheck
+    currentCheck++
+    // get the newHeight (height is changed just before browser repaint)
+    let newHeight = `${el.clientHeight}px`
+    // check to see if newHeight is the same as oldHeight, if so, vue is still adding
+    // or removing a child element and the parent has not changed height yet.
+    if (newHeight === oldHeight) {
+      // If heights are the same, loop this function just before each browser repaint.
+      // NUXT/SSR warning, make sure this directive only runs browserside, or
+      // wrap the following line in an if (process.browser) { // here }
+      window.requestAnimationFrame(check)
+    } else {
+      // Otherwise, the height has changed and we're in limbo before browser has visualised the changes.
+      // So let's catch that and set the element's height to the old height before the change.
       el.style.height = oldHeight
-      // use setTimeout to delay setting the element's height to the new height
+      // Then we'll delay setting the new height as per the user's delay setting
       setTimeout(() => {
+        // when the delay is finished, set the height of the element to the new height
         el.style.height = newHeight
+        // wait until the height change transition has finished as per the user's transition setting.
+        setTimeout(() => {
+          // change the element's height back to 'auto' so that
+          // it's height will change again the next time a child is added or removed.
+          el.style.height = 'auto'
+        }, transitionTime)
       }, delay)
-    })
+    }
+  }
+  // start the looping function
+  check()
+}
+
+const smoothResize = (el, binding, vnode) => {
+  // first things first, set the element's height to auto so that
+  // it's height will change the next time a child is added or removed.
+  el.style.height = 'auto'
+  // transitionTime is the amount of time in ms that the transition should take.
+  let transitionTime = (binding.value && binding.value.transition) || 300
+  transitionTime = parseInt(transitionTime)
+  // fineTune is to adjust how many browser repaints to loop for in checkHeightForChangeAndUpdate()
+  let fineTune = (binding.value && binding.value.fineTune) || 20
+  fineTune = parseInt(fineTune)
+  // delay is to delay the setting of the newHeight
+  let delay = (binding.value && binding.value.delay) || 0
+  delay = parseInt(delay)
+  // get the old height
+  let oldHeight = `${el.clientHeight}px`
+  // set the transition and overflow styles.
+  el.style.transition = `height ${transitionTime}ms cubic-bezier(0.25, 0.8, 0.5, 1)`
+  el.style.overflow = 'hidden'
+  // Run the function to check for height change and adjust accordingly
+  checkHeightForChangeAndUpdate(el, oldHeight, delay, transitionTime, fineTune)
+}
+
+Vue.directive('smooth-resize', {
+  componentUpdated (el, binding, vnode) {
+    smoothResize(el, binding, vnode)
+  },
+  updated (el, binding, vnode) {
+    smoothResize(el, binding, vnode)
   }
 })
